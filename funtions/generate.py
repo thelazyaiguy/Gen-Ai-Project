ec9c90e7d1eaf593008813a1a4cd91f652f885d9
thelazyaiguy <stolenandfound148@gmail.com>
2024-10-06 16:08:13 +0500

    Removed API key for security

--- funtions/generate.py
+++ funtions/generate.py
@@ -2,8 +2,7 @@ import json
 import openai
 
 # Set up your OpenAI API key
-openai.api_key = "sk-svcacct-OAI6Jicdzf3XOPHHwaXvJeIOFwpB5E3hALWSBRxTUpatnD7GVTUb7mPkri83A6T3BlbkFJz-Ry6AyxT6QdvUAEJL7xNjhmVE0055OvJcnIxX8W2u1w3PzLUGRYfQhDsOdnYA"
-
+# openai.api_key = "sk-svcacct-OAI6Jicdzf3XOPHHwaXvJeIOFwpB5E3hALWSBRxTUpatnD7GVTUb7mPkri83A6T3BlbkFJz-Ry6AyxT6QdvUAEJL7xNjhmVE0055OvJcnIxX8W2u1w3PzLUGRYfQhDsOdnYA"
 def handler(event, context):
     try:
         # Parse the data from the request body
